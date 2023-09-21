19e069e502f1dd5be15a51a5da082d1afdca1cf7
raechelann <raechel.curiel@gmail.com>
2023-09-21 16:27:37 -0400

    adding character getName method

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -1,5 +1,10 @@
+import string
+
 class Character:
     name = ""
 
     def __init__(self, character_name):
         self.name = character_name
+
+    def getName(self) -> string:
+        return self.name
--- tests/test_character.py
+++ tests/test_character.py
@@ -6,3 +6,9 @@ class TestCharacterInitWithName(TestCase):
         ARBITRARY_NAME = "MyName"
         testobj = Character(ARBITRARY_NAME)
         self.assertEqual(ARBITRARY_NAME, testobj.name)
+
+    def test_get_name(self):
+        ARBITRARY_NAME = "FooBar"
+        testobj = Character(ARBITRARY_NAME)
+        result = testobj.getName()
+        self.assertEqual(ARBITRARY_NAME, result)
